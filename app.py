3b11c72ecd03f76fade6e90a91fb659474d0ce60
innatives <51743162+innatives@users.noreply.github.com>
2022-05-28 13:55:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ st.set_option('deprecation.showfileUploaderEncoding', False)
 tflite_interpreter = tf.lite.Interpreter(model_path='saved_model.tflite')
 tflite_interpreter.allocate_tensors()
 
+uploaded_file = st.file_uploader("Upload a Image", type=["jpg","png", 'jpeg'])
+
 def load_img(path):
         ## reading file object and making it to pil image and to np array
         img_l=[]
@@ -23,7 +25,7 @@ def load_img(path):
                 img_arr=np.expand_dims(img_arr,axis=-1)
                 img_l.append(img_arr)
         img=np.stack(img_l)
-        return input_image
+        return img
 
 def set_input_tensor(interpreter, image):
     """Sets the input tensor."""
@@ -31,9 +33,9 @@ def set_input_tensor(interpreter, image):
     input_tensor = interpreter.tensor(tensor_index)()[0]
     input_tensor[:, :] = image
 
-def get_predictions(input_image):
+def get_predictions(img):
     output_details = tflite_interpreter.get_output_details()
-    set_input_tensor(tflite_interpreter, input_image)
+    set_input_tensor(tflite_interpreter, img)
     tflite_interpreter.invoke()
     tflite_model_prediction = tflite_interpreter.get_tensor(output_details[0]["index"])
     tflite_model_prediction = tflite_model_prediction.squeeze().argmax(axis = 0)
@@ -49,7 +51,6 @@ def load_model():
 st.title('Choroby pomidorów')
 
 ## Input Fields
-uploaded_file = st.file_uploader("Upload a Image", type=["jpg","png", 'jpeg'])
 
 if uploaded_file is not None:		
     img = Image.open(uploaded_file)
@@ -58,5 +59,5 @@ if uploaded_file is not None:
     img_array = tf.expand_dims(img_array, 0)
 
 if st.button("Sprawdź pomidora"):
-    suggestion = get_predictions(input_image =img_array)
+    suggestion = get_predictions(img =img_array)
     st.success(suggestion)
