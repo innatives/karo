f67d30f569afbc6e73a57e9341055b1c98094547
innatives <51743162+innatives@users.noreply.github.com>
2022-05-28 11:44:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -37,11 +37,9 @@ file = st.file_uploader("Upload an image of a flower", type=["jpg", "png"])
 ## Input Fields
 uploaded_file = st.file_uploader("Upload a Image", type=["jpg","png", 'jpeg'])
 if uploaded_file is not None:
-    img = tf.keras.preprocessing.image.load_img(path , grayscale=False, color_mode='rgb', target_size=(224,224,3), interpolation='nearest')
-    st.image(img)
+    img = tf.keras.preprocessing.image.load_img(path, target_size=(224,224,3))
+    st.image(img, caption="Input Image", width = 400)
     print(value)
-    if value == 2 or value == 5:
-        img = tf.image.convert_image_dtype(img, tf.uint8)
     img_array = tf.keras.preprocessing.image.img_to_array(img)
     img_array = tf.expand_dims(img_array, 0)
 
