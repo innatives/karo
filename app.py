667c9bfccb27f93966e808d0e0c9f39604caf6db
innatives <51743162+innatives@users.noreply.github.com>
2022-05-28 09:29:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.set_option('deprecation.showfileUploaderEncoding', False)
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-	model = tf.keras.models.load_model('./saved_model.tflite')
+	model = tf.keras.models.load_model('./flower_model_trained.hdf5')
 	return model
 
 
