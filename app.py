6bbab32393a7225a7d0ce156a3476c83b11cd4a0
innatives <51743162+innatives@users.noreply.github.com>
2022-05-28 10:30:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,50 +6,70 @@ from PIL import Image
 
 st.set_option('deprecation.showfileUploaderEncoding', False)
 
-@st.cache(allow_output_mutation=True)
-def load_model():
-	model = tf.lite.Interpreter(model_path = r'saved_model.tflite')
-	return model
 
+## prediction
+@st.cache(show_spinner=False)
+def pred(img):
+    # Load TFLite model and allocate tensors.
+    interpreter = tf.lite.Interpreter(model_path = r'saved_model.tflit')
 
-def predict_class(image, model):
+    # setting input size
+    interpreter.resize_tensor_input(0, [img.shape[0],244,244,1], strict=True)
+    interpreter.allocate_tensors()
+    #interpreter = load_model()
+    # Get input and output tensors.
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+    # Test the TensorFlow Lite model on random input data.
+    input_shape = input_details[0]['shape']
 
-	image = tf.cast(image, tf.float32)
-	image = tf.image.resize(image, [224, 224])
+    input_data = img
+    interpreter.set_tensor(input_details[0]['index'], input_data)
 
-	image = np.expand_dims(image, axis = 0)
+    interpreter.invoke()
+    
+    # making predictions
+    tflite_results = interpreter.get_tensor(output_details[0]['index'])
 
-	prediction = model.predict(image)
+    #tflite_resluts = (tflite_results)
 
-	return prediction
+    tflite_results = np.concatenate(tflite_results)  # merging all sub arrays
+    
+    tf_results=[1 if i>0.5 else 0 for i in tflite_results]  # scaling predictions to 0,1.
+ 
+    return tflite_results,tf_results
 
+vis_img = st.sidebar.checkbox('Show Uploaded Images')
 
-model = load_model()
-st.title('Flower Classifier')
+# if file is uploaded
+if inp_t:
+        img = load_img(inp_t)
+        
+        st.warning('** Uploaded {} images [View images in side Panel]'.format(img.shape[0]))
+         
+        res_prob,res = np.array(pred(img))  # convert predictions list to array
 
-file = st.file_uploader("Upload an image of a flower", type=["jpg", "png"])
+        fig,ax=plt.subplots()
+          
+        for i in range(len(res)):
+                
+                if res[i] == 0:
+                  pred_conf = (0.5 - res_prob[i]) / 0.5
+                  pred_conf = pred_conf * 100
+                else:
+                  pred_conf = res_prob[i] * 100
+                  
+                st.subheader("*Image "+str(i+1)+" : Model predicts there is {}  tumor with [{} % confidence].*".format(class_labels[res[i]],round(pred_conf,2)))
+                
+                #if st.checkbox('View Image - ' +str(i+1)):
+                           #st.image(img[i],use_column_width=True)
+                st.write('\n')
+                    
+                if vis_img:
+                        st.sidebar.write('{} - Image Dimensions: {}'.format(str(i+1),img[i].shape))
+                        st.sidebar.image(img[i],use_column_width=True)
+        st.markdown('---')
+        st.error('Dont conclude by looking at predictions, just take them as a reference!!')
 
-
-if file is None:
-	st.text('Waiting for upload....')
-
-else:
-	slot = st.empty()
-	slot.text('Running inference....')
-
-	test_image = Image.open(file)
-
-	st.image(test_image, caption="Input Image", width = 400)
-
-	pred = predict_class(np.asarray(test_image), model)
-
-	class_names = ['Bakteria', 'Mozaika', 'Pleśń', 'Zaraza ziemniaczana']
-
-	result = class_names[np.argmax(pred)]
-
-	output = 'The image is a ' + result
-
-	slot.text('Done')
-
-	st.success(output)
-
+## prints model arch flow chart
+#if st.sidebar.checkbox('Model Architecture'):            
