f2939beda552f29fabb620dbfd9785ba93e4ea17
innatives <51743162+innatives@users.noreply.github.com>
2022-05-28 09:16:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.set_option('deprecation.showfileUploaderEncoding', False)
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-	model = tf.keras.models.load_model('./saved_model.pb')
+	model = tf.keras.models.load_model('./flower_model_trained.hdf5')
 	return model
 
 
@@ -43,7 +43,7 @@ else:
 
 	pred = predict_class(np.asarray(test_image), model)
 
-	class_names = ['Bakteria', 'Mozaika', 'Pleśń', 'Zaraza ziemniaczana']
+	class_names = ['daisy', 'dandelion', 'rose', 'sunflower', 'tulip']
 
 	result = class_names[np.argmax(pred)]
 
