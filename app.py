8c4321c1722cd115b82a11b1b751e3f9a9887449
innatives <51743162+innatives@users.noreply.github.com>
2022-05-28 10:55:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,22 @@ def load_model():
 	model = tf.keras.models.load_model('./flower_model_trained.hdf5')
 	return model
 
+#load image
+@st.cache(show_spinner=False)
+def load_img(path):
+        ## reading file object and making it to pil image and to np array
+        img_l=[]
+        for i in path:
+                img_byte=i.read()
+                img=Image.open(io.BytesIO(img_byte))
+                img=img.resize((256,256),Image.ANTIALIAS)
+                if img.mode!='L':
+                        img=img.convert('L')
+                img_arr=np.array(img,dtype='float32')/255
+                img_arr=np.expand_dims(img_arr,axis=-1)
+                img_l.append(img_arr)
+        img=np.stack(img_l)
+        return img
 
 ## prediction
 @st.cache(show_spinner=False)
