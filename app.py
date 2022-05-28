85623f62077f7b95c10e07dff44c6d77be3eba97
innatives <51743162+innatives@users.noreply.github.com>
2022-05-28 11:56:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -42,6 +42,6 @@ if uploaded_file is not None:
     img_array = tf.keras.utils.img_to_array(img)
     img_array = tf.expand_dims(img_array, 0)
 
-suggestion = get_predictions(input_image =img_array)
-st.success(suggestion)
-
+if st.button("Sprawdź pomidora"):
+    suggestion = get_predictions(input_image =img_array)
+    st.success(suggestion)
