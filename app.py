1f66c62c853ca6dcbfb89f5bcab18d9f7e871e8c
innatives <51743162+innatives@users.noreply.github.com>
2022-05-28 11:26:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,24 @@ from PIL import Image
 
 st.set_option('deprecation.showfileUploaderEncoding', False)
 
+    tflite_interpreter = tf.lite.Interpreter(model_path='saved_model.tflite')
+    tflite_interpreter.allocate_tensors()
+
+def set_input_tensor(interpreter, image):
+    """Sets the input tensor."""
+    tensor_index = interpreter.get_input_details()[0]['index']
+    input_tensor = interpreter.tensor(tensor_index)()[0]
+    input_tensor[:, :] = image
+
+def get_predictions(input_image):
+    output_details = tflite_interpreter.get_output_details()
+    set_input_tensor(tflite_interpreter, input_image)
+    tflite_interpreter.invoke()
+    tflite_model_prediction = tflite_interpreter.get_tensor(output_details[0]["index"])
+    tflite_model_prediction = tflite_model_prediction.squeeze().argmax(axis = 0)
+    pred_class = class_names[tflite_model_prediction]
+    return pred_class
+
 @st.cache(allow_output_mutation=True)
 def load_model():
 	model = tf.keras.models.load_model('./flower_model_trained.hdf5')
