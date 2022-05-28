665d23fc7ce71920e06f230c42fda94fd396a979
innatives <51743162+innatives@users.noreply.github.com>
2022-05-28 14:30:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -44,5 +44,5 @@ if uploaded_file is not None:
     st.image(img, caption="Input Image", width = 400)
 
 if st.button("Sprawdź pomidora"):
-    suggestion = get_predictions(img =img_array)
+    suggestion = get_predictions(input_image =img_array)
     st.success(suggestion)
