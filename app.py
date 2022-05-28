89db7538550020276bf6804ec9d7cbf7d1fb351f
innatives <51743162+innatives@users.noreply.github.com>
2022-05-28 13:31:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ uploaded_file = st.file_uploader("Upload a Image", type=["jpg","png", 'jpeg'])
 if uploaded_file is not None:		
     img = Image.open(uploaded_file)
     st.image(img, caption="Input Image", width = 400)
-    img_array = tf.keras.utils.img_to_array(img)
+    img_array = tf.keras.utils.img_to_array(img, target_size=(224,224))
     img_array = tf.expand_dims(img_array, 0)
 
 if st.button("Sprawdź pomidora"):
