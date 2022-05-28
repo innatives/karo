8d37af24478e3e1553e28982ebfb0a40d153403e
innatives <51743162+innatives@users.noreply.github.com>
2022-05-28 10:53:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,38 @@ def load_model():
 	model = tf.keras.models.load_model('./flower_model_trained.hdf5')
 	return model
 
-interpreter = tf.lite.Interpreter(model_path='saved_model.tflite')
-interpreter.allocate_tensors()
+
+## prediction
+@st.cache(show_spinner=False)
+def pred(img):
+    # Load TFLite model and allocate tensors.
+    interpreter = tf.lite.Interpreter(model_path = r'saved_model.tflite')
+
+    # setting input size
+    interpreter.resize_tensor_input(0, [img.shape[0],256,256,1], strict=True)
+    interpreter.allocate_tensors()
+    #interpreter = load_model()
+    # Get input and output tensors.
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+    # Test the TensorFlow Lite model on random input data.
+    input_shape = input_details[0]['shape']
+
+    input_data = img
+    interpreter.set_tensor(input_details[0]['index'], input_data)
+
+    interpreter.invoke()
+    
+    # making predictions
+    tflite_results = interpreter.get_tensor(output_details[0]['index'])
+
+    #tflite_resluts = (tflite_results)
+
+    tflite_results = np.concatenate(tflite_results)  # merging all sub arrays
+    
+    tf_results=[1 if i>0.5 else 0 for i in tflite_results]  # scaling predictions to 0,1.
+ 
+    return tflite_results,tf_results
 
 def predict_class(image, model):
 
